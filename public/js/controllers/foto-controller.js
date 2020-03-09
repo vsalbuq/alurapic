c443010ac15eba2dc03d182733bc41f2fe47d6cd
vsalbuq <vinicius.albuquerque@outlook.com>
2020-03-09 15:27:40 -0300

    feat: adds new photo feature

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,7 +1,21 @@
-angular.module("alurapic").controller("FotoController", function($scope) {
-  $scope.foto = {};
+angular
+  .module("alurapic")
+  .controller("FotoController", function($scope, $http) {
+    $scope.foto = {};
+    $scope.mensagem = "";
 
-  $scope.submeter = function() {
-    console.log($scope.foto);
-  };
-});
+    $scope.submeter = function() {
+      if ($scope.formulario.$valid) {
+        $http
+          .post("v1/fotos", $scope.foto)
+          .success(function() {
+            $scope.foto = {};
+            $scope.mensagem = "Foto cadastrada com sucesso";
+          })
+          .error(function(err) {
+            $scope.mensagem = "Não foi possível incluir a foto";
+            console.error(err);
+          });
+      }
+    };
+  });
